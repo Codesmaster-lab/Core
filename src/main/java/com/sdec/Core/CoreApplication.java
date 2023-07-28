66b9d90dd358be70e36d3b1626dc02fa39b93cf4
Souvikdas-openlm <souvik.das@openlm.com>
2023-07-28 14:54:43 +0530

    Testing the Git on Intellij

--- src/main/java/com/sdec/Core/CoreApplication.java
+++ src/main/java/com/sdec/Core/CoreApplication.java
@@ -2,11 +2,19 @@ package com.sdec.Core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CoreApplication {
-
-	public static void main(String[] args) {
+	@PostMapping("/test")
+	public String testApi()
+	{
+		System.out.println("hello world");
+		return ("Hello World");
+	}
+	public static void main(String[] args)
+	{
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
