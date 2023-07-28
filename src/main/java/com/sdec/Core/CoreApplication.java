1eb81fa8451ac8d8f4986f3d6b29a3dc6fc58004
Souvikdas-openlm <souvik.das@openlm.com>
2023-07-28 17:20:14 +0530

    Changed from Gradle to Maven

--- src/main/java/com/sdec/Core/CoreApplication.java
+++ src/main/java/com/sdec/Core/CoreApplication.java
@@ -2,30 +2,12 @@ package com.sdec.Core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class CoreApplication {
-	/**
-	 * To test the data appearing on the webpage
-	 */
-	@RestController
-	public static class Souvik
-	{
-		@RequestMapping("/test")
-		public String hello()
-		{
-			return "Hello Souvik";
-		}
-	}
-	public static void main(String[] args)
-	{
-		SpringApplication.run(CoreApplication.class, args);
 
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
 	}
 
 }
